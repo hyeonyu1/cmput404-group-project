c0e018a682edd2e8dde324ce6fd8ad5b8d64b9eb
Cole Boytinck <cboytinc@ualberta.ca>
2020-01-28 12:34:51 -0700

    Added needed models from the swagger_doc Generated for flask using SwaggerHub

---
+++ social_distribution/social_distribution/models/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from .array_of_comments import ArrayOfComments
+from .array_of_posts import ArrayOfPosts
+from .author import Author
+from .body import Body
+from .body1 import Body1
+from .body2 import Body2
+from .body3 import Body3
+from .comment import Comment
+from .extend_author_model import ExtendAuthorModel
+from .friendrequest_author import FriendrequestAuthor
+from .friendrequest_friend import FriendrequestFriend
+from .inline_response200 import InlineResponse200
+from .inline_response2001 import InlineResponse2001
+from .inline_response2002 import InlineResponse2002
+from .inline_response2003 import InlineResponse2003
+from .inline_response2004 import InlineResponse2004
+from .inline_response2005 import InlineResponse2005
+from .inline_response2006 import InlineResponse2006
+from .inline_response2007 import InlineResponse2007
+from .inline_response403 import InlineResponse403
+from .post import Post
