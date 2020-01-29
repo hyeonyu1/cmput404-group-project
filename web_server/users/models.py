1dd2ffd52a6816dd3bbab852ff6798b7f8774d9b
Austin Crapo <corey.crapo@gmail.com>
2020-01-29 11:46:25 -0700

    Translate the autogenerated user model into a django app and model

---
+++ web_server/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class User(models.Model):
+    email = models.CharField(max_length=256)  # used as username?
+    bio = models.CharField(max_length=256)
+    host = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    display_name = models.CharField(max_length=256)
+    url = models.CharField(max_length=256)
+    github = models.CharField(max_length=256)
