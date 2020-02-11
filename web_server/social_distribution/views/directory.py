0ab06c96e9af5223221025fa64a74e75c69e429a
Dylan Alcock <dylan.alcock@gmail.com>
2020-02-10 23:09:40 -0700

    Attempting to get posts working

--- web_server/social_distribution/views/directory.py
+++ web_server/social_distribution/views/directory.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 # This file is used to display web pages
 
 def main_page(request):
-    return render(request, 'index.html')
+    return render(request, 'mainPage.html')
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'mainPage.html', {'form': form})
