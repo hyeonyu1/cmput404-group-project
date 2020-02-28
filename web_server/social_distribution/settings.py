a8367a2f1a715b5bf3895b2692dcc71bc0d91611
AustinGrey <corey.crapo@gmail.com>
2020-02-28 10:09:57 -0700

    Set collectstatic settings properly

--- web_server/social_distribution/settings.py
+++ web_server/social_distribution/settings.py
@@ -130,8 +130,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# STATIC_URL = '/static/'
 
+## CODE FROM https://devcenter.heroku.com/articles/django-assets
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+## END CODE FROM HEROKU
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
