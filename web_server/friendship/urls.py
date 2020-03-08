abd6a2e61e3c6bb0ba1d0b2a7ef2aed1c1bcef2a
PhoebeBai <ybai5@ualberta.ca>
2020-03-07 17:45:03 -0700

    display friendrequest and handle friendrequest

--- web_server/friendship/urls.py
+++ web_server/friendship/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.send_friend_request),
-    path('handle', views.handle_friend_request),
+    path('handle', views.handle_friend_request, name = 'handle'),
     path('<str:author_id>', views.retrieve_friend_request_of_author_id)
 
 
