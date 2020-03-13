0ae9a1e2c983da191a08e91fe6d8d23054995790
Hyeon Yu <hyeon@ualberta.ca>
2020-03-13 13:38:26 -0600

    changed error code for updating profile and if the user is not logged in takes the user to a log in page

--- web_server/social_distribution/views.py
+++ web_server/social_distribution/views.py
@@ -5,6 +5,10 @@ from django.urls import reverse
 
 def home(request):
     if request.method == 'GET':
-        return redirect(reverse('stream'))
+        # return redirect(reverse('stream'))
+        if request.user.is_authenticated:
+            return redirect(reverse('stream'))
+        else:
+            return redirect(reverse('login'))
     else:
         return HttpResponse('404 Error', status=404)
