52860f0e7bad335d8a26214245b1c0298eb9f705
XuechunHou <xhou1@ualberta.ca>
2020-03-24 22:58:25 -0600

    removed trailing slash

--- web_server/friendship/urls.py
+++ web_server/friendship/urls.py
@@ -3,9 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.send_friend_request,
          name="send_friend_request"),
-    path('handle', views.handle_friend_request,
+    path('/handle', views.handle_friend_request,
          name="handle_friend_request"),
-    path('<str:author_id>', views.retrieve_friend_request_of_author_id,
+    path('/<str:author_id>', views.retrieve_friend_request_of_author_id,
          name="retrieve_friend_request_of_author_id")
 
 
