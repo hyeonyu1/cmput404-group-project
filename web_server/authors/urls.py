782d04d666ae4c9702b52d940fce4a062eedde0f
XuechunHou <xhou1@ualberta.ca>
2020-03-27 12:47:53 -0600

    removed unecessary comments

--- web_server/authors/urls.py
+++ web_server/authors/urls.py
@@ -15,20 +15,18 @@ urlpatterns = [
 
 
     path('<path:author_id>/friends/', views.friend_checking_and_retrieval_of_author_id,
-         name="friend_checking_and_retrieval_of_author_id"),  # works with and wo slash
+         name="friend_checking_and_retrieval_of_author_id"),
     path('<str:author1_id>/friends/<path:author2_id>/',
-         views.check_if_two_authors_are_friends, name="check_if_two_authors_are_friends"),  # works with and wo slash
+         views.check_if_two_authors_are_friends, name="check_if_two_authors_are_friends"),
     path('<str:author_id>/', views.retrieve_author_profile,
-         name="retrieve_author_profile"),  # works with and wo slash
+         name="retrieve_author_profile"),
     path('', views.return_all_authors_registered_on_local_server,
          name="retrieve_all_authors"),
     path('<str:author_id>/update', views.update_author_profile,
-         name="update_author_profile"),  # not used
+         name="update_author_profile"),
     path('unfriend', views.unfriend, name="unfriend"),  # only work wo slash
     path('<str:author_id>/addfriend/', views.view_list_of_available_authors_to_befriend,
-         name="view_list_of_available_authors_to_befriend"),  # works with and wo slash
-
-    # Internal Use API's
+         name="view_list_of_available_authors_to_befriend"),
     path('', views.get_all_authors, name='all_authors'),
 
     # Webpage URLS
