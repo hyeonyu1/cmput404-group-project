8ee887851a2f6a37b33e33f21e0316f37e72f143
Austin Crapo <corey.crapo@gmail.com>
2020-04-02 09:37:50 -0600

    Added the ability to see display name of each visibleTo

--- web_server/authors/static/javascript/posting.js
+++ web_server/authors/static/javascript/posting.js
@@ -34,9 +34,26 @@ function get_all_users(selectedUsers){
 						opt.selected = 'selected';
 					}
 				}
-				select.appendChild(opt)
+				select.appendChild(opt);
 			}
+			get_visible_to_names(select);
 		})
+}
+
+/**
+ * For the given select element, will cycle through all of it's options and if the
+ * inner text is a valid profile link, then it will replace the inner text with the users display name and host
+ * @param select_element
+ */
+function get_visible_to_names(select_element){
+	let options = select_element.querySelectorAll('option');
+	for(let option of options){
+		fetch('/author/profile/' + option.innerText)
+			.then(response => response.json())
+			.then(data => {
+				option.innerText = data.displayName + ' (' + option.innerText + ')'
+			})
+	}
 }
 
 
