09f5ca71f77867a03805aca6f1822236c26c0ee4
Hyeon Yu <hyeon@ualberta.ca>
2020-04-04 16:12:44 -0600

    comment

--- web_server/authors/views.py
+++ web_server/authors/views.py
@@ -452,17 +452,16 @@ def post_creation_and_retrieval_to_curr_auth_user(request):
 
     # Response for a local user, will get all the posts that the user can see, including friends, and foaf
     def retrieve_posts(request):
-        user_uid = url_regex.sub("", request.user.uid).rstrip("/")
         # own post
         own_post = Post.objects.filter(
-            author_id=user_uid, unlisted=False)
+            author_id=request.user.uid, unlisted=False)
 
         # visibility =  PUBLIC
         public_post = Post.objects.filter(visibility="PUBLIC", unlisted=False)
 
         # visibility = FRIENDS
         users_friends = []
-        friends = Friend.objects.filter(author_id=user_uid)
+        friends = Friend.objects.filter(author_id=request.user.uid)
         for friend in friends:
             users_friends.append(friend.friend_id)
         friend_post = Post.objects.filter(
@@ -478,7 +477,7 @@ def post_creation_and_retrieval_to_curr_auth_user(request):
 
         # visibility = PRIVATE
         private_post = Post.objects.filter(
-            visibleTo=user_uid, unlisted=False)
+            visibleTo=request.user.uid, unlisted=False)
 
         # visibility = SERVERONLY
         local_host = request.user.host
