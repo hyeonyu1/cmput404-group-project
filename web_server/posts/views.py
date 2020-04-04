3af93b4d9b175403e3b1f2cd7614b22a7e40e83c
Austin Crapo <corey.crapo@gmail.com>
2020-04-04 10:39:16 -0600

    Fix the bug

--- web_server/posts/views.py
+++ web_server/posts/views.py
@@ -85,7 +85,7 @@ def retrieve_single_post_with_id(request, post_id):
             # getting the friends of the author
             return FOAF_verification(request, author_id)
         elif visibility == Post.SERVERONLY:
-            if request.user.host == Author.objects.get(id=author_id).host:
+            if request.user.host == Author.objects.get(uid=author_id).host:
                 return True
         elif visibility == Post.PRIVATE:
             # The visibleTo list contains protocols, which we dont want
