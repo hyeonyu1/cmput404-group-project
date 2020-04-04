8ebf87f5a34117a0bee85f5f53386539ee62bc52
Hyeon Yu <hyeon@ualberta.ca>
2020-04-03 21:07:39 -0600

    comment

--- web_server/users/urls.py
+++ web_server/users/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('addfriend/', views.add_friend, name='add_friend'),
 
-    path('view_post/<path:post_path>/comments', views.view_post_comment, name='view_post_comment'),
+    # path('view_post/<path:post_path>/comments', views.view_post_comment, name='view_post_comment'),
     path('view_post/<path:post_path>/', views.view_post, name='view_post')
 ]
