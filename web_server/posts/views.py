6595c9304a9ed4cecbc75d35386cbdf0abf1cff5
Austin Crapo <corey.crapo@gmail.com>
2020-04-05 17:25:52 -0600

    Fix typo in indentation

--- web_server/posts/views.py
+++ web_server/posts/views.py
@@ -70,15 +70,16 @@ def retrieve_single_post_with_id(request, post_id):
         visibility = api_object_post["visibility"]
         # Foreign servers can access all posts, unless they are 'SERVERONLY',
         # or if image or post sharing has been turned off
-        if request.remote_server_authenticated and visibility != Post.SERVERONLY:
-            allowed_post_types = []
-            if request.remote_server_authenticated_for_images:
-                allowed_post_types += [Post.TYPE_JPEG, Post.TYPE_PNG]
-            if request.remote_server_authenticated_for_posts:
-                allowed_post_types += [Post.TYPE_BASE64, Post.TYPE_MARKDOWN, Post.TYPE_PLAIN]
-            return api_object_post['contentType'] in allowed_post_types
-        else:
-            return False
+        if request.remote_server_authenticated:
+            if visibility != Post.SERVERONLY:
+                allowed_post_types = []
+                if request.remote_server_authenticated_for_images:
+                    allowed_post_types += [Post.TYPE_JPEG, Post.TYPE_PNG]
+                if request.remote_server_authenticated_for_posts:
+                    allowed_post_types += [Post.TYPE_BASE64, Post.TYPE_MARKDOWN, Post.TYPE_PLAIN]
+                return api_object_post['contentType'] in allowed_post_types
+            else:
+                return False
 
         user_id = request.user.uid
         author_id = api_object_post["author"]['id']
