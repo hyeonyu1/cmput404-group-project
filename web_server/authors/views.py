56f5872acbdf0d5c6ac351cf5f40620db493920e
Hyeon Yu <hyeon@ualberta.ca>
2020-04-05 17:26:01 -0600

    comment

--- web_server/authors/views.py
+++ web_server/authors/views.py
@@ -287,10 +287,12 @@ def retrieve_universal_author_profile(request, author_id):
 
 def retrieve_author_profile(request, author_id):
     if request.method == 'GET':
+        print("AUTH_PROFILE")
         # compose full url of author
         host = request.get_host()
         # author_id = host + "/author/" + str(author_id)
 
+        author_uid = url_regex.sub("", author_id).rstrip("/")
         # only active authors are retrivable
         author = get_object_or_404(
             Author.objects.filter(is_active=1), uid=author_id)
