06d65aa83e0155fd6d6c7f5128cbc5412a1ae312
Hyeon Yu <hyeon@ualberta.ca>
2020-04-05 23:50:40 -0600

    comment

--- web_server/authors/views.py
+++ web_server/authors/views.py
@@ -560,6 +560,7 @@ def post_creation_and_retrieval_to_curr_auth_user(request):
 
     # Response for a local user, will get all the posts that the user can see, including friends, and foaf
     def retrieve_posts(request):
+        print("\n\n\n\n\n in retrive_post")
         # own post
         own_post = Post.objects.filter(
             author_id=request.user.uid, unlisted=False)
